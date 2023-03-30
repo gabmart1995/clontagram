da1b5d29cbdcf816097c4c4e436120eeccc59579
gabmart1995 <gabmart1995@gmail.com>
2023-03-30 19:53:41 -0400

    wip: ajustes BD

--- clontagram.sql
+++ clontagram.sql
@@ -1,215 +1,115 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.11.2-MariaDB, for Win64 (AMD64)
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 28, 2022 at 06:36 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.4.16
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: clontagram
+-- ------------------------------------------------------
+-- Server version	10.11.2-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `clontagram`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `comments`
 --
 
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` text NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
-  `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
   `userId` int(11) DEFAULT NULL,
-  `imageId` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `comments`
---
-
-/*INSERT INTO `comments` (`id`, `content`, `createdAt`, `updatedAt`, `userId`, `imageId`) VALUES
-(1, 'Guau que hermosa foto', '2022-04-21 15:14:00', '0000-00-00 00:00:00', 2, 1),
-(2, 'verdad que si.', '2022-04-21 15:14:00', '0000-00-00 00:00:00', 1, 1),
-(4, 'que bien', '2022-04-22 10:26:18', '0000-00-00 00:00:00', 1, 4),
-(7, 'test de desarrollo', '2022-04-26 10:55:07', '2022-04-26 10:55:07', 1, 11),
-(8, 'wip.', '2022-04-26 10:57:19', '2022-04-26 10:57:19', 2, 13),
-(9, 'que lindo pinguino', '2022-04-26 11:42:36', '2022-04-26 11:42:36', 1, 14),
-(10, 'guau', '2022-04-28 08:46:50', '2022-04-28 08:46:50', 1, 5);*/
-
--- --------------------------------------------------------
+  `imageId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_7e8d7c49f218ebb14314fdb3749` (`userId`),
+  KEY `FK_6508f19beedb8e87c3a7d2df579` (`imageId`),
+  CONSTRAINT `FK_6508f19beedb8e87c3a7d2df579` FOREIGN KEY (`imageId`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `FK_7e8d7c49f218ebb14314fdb3749` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `images`
 --
 
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `images` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `imagePath` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
-  `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
-  `userId` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `images`
---
-
-/*INSERT INTO `images` (`id`, `imagePath`, `description`, `createdAt`, `updatedAt`, `userId`) VALUES
-(1, 'http://[::1]:3000/uploads/images/image_path-1650571062128-978585088.jpeg', 'mi primera sesion de fotografia de paisajes', '2022-04-21 15:14:00', '0000-00-00 00:00:00', 1),
-(3, 'http://[::1]:3000/uploads/images/image_path-1650578088792-273106544.jpeg', 'Vacaciones en Alaska con los abuelos', '2022-04-21 15:14:00', '0000-00-00 00:00:00', 1),
-(4, 'http://[::1]:3000/uploads/images/image_path-1650637270378-430937362.png', 'aprendiendo golang', '2022-04-22 10:21:10', '0000-00-00 00:00:00', 1),
-(5, 'http://[::1]:3000/uploads/images/image_path-1651150199727-71735701.jpeg', 'foto de un colibri en mi jardin', '2022-04-24 17:06:35', '2022-04-28 08:59:00', 1),
-(7, 'http://[::1]:3000/uploads/images/image_path-1650834915140-49965024.png', 'logo de perfil', '2022-04-24 17:15:15', '0000-00-00 00:00:00', 1),
-(8, 'http://[::1]:3000/uploads/images/image_path-1650940957700-99533861.png', 'desarrollo productos app', '2022-04-25 22:42:37', '0000-00-00 00:00:00', 2),
-(13, 'http://[::1]:3000/uploads/images/image_path-1650985019738-111044795.png', 'Trabajando en mi proximo proyecto', '2022-04-26 10:56:59', '2022-04-28 08:53:00', 2);*/
-
--- --------------------------------------------------------
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `userId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_96514329909c945f10974aff5f8` (`userId`),
+  CONSTRAINT `FK_96514329909c945f10974aff5f8` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `likes`
 --
 
+DROP TABLE IF EXISTS `likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `likes` (
-  `id` int(11) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
-  `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
   `userId` int(11) DEFAULT NULL,
-  `imageId` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `likes`
---
-
-/*INSERT INTO `likes` (`id`, `createdAt`, `updatedAt`, `userId`, `imageId`) VALUES
-(28, '2022-04-23 15:19:47', '0000-00-00 00:00:00', 2, 1),
-(30, '2022-04-23 18:45:56', '0000-00-00 00:00:00', 1, 1),
-(45, '2022-04-24 09:15:14', '0000-00-00 00:00:00', 1, 3),
-(46, '2022-04-24 09:16:33', '0000-00-00 00:00:00', 2, 4),
-(48, '2022-04-24 17:07:53', '0000-00-00 00:00:00', 2, 5),
-(49, '2022-04-24 17:15:31', '0000-00-00 00:00:00', 1, 7),
-(50, '2022-04-24 17:15:44', '0000-00-00 00:00:00', 2, 7),
-(53, '2022-04-25 22:42:45', '0000-00-00 00:00:00', 2, 8),
-(54, '2022-04-25 22:42:58', '0000-00-00 00:00:00', 1, 8),
-(58, '2022-04-26 10:57:10', '2022-04-26 10:57:10', 2, 13),
-(59, '2022-04-26 10:58:12', '2022-04-26 10:58:12', 1, 13),
-(61, '2022-04-28 08:50:09', '2022-04-28 08:50:09', 1, 5);
-*/
--- --------------------------------------------------------
+  `imageId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_cfd8e81fac09d7339a32e57d904` (`userId`),
+  KEY `FK_d2361de1b2474386782a4031731` (`imageId`),
+  CONSTRAINT `FK_cfd8e81fac09d7339a32e57d904` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_d2361de1b2474386782a4031731` FOREIGN KEY (`imageId`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(20) NOT NULL DEFAULT 'user',
   `name` varchar(100) NOT NULL,
   `surname` varchar(200) NOT NULL,
   `nick` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
-  `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
-  `rememberToken` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `rememberToken` varchar(255) DEFAULT NULL,
+  `image` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nick-unique` (`nick`),
+  UNIQUE KEY `email-unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Dumping data for table `users`
---
-
-/*INSERT INTO `users` (`id`, `role`, `name`, `surname`, `nick`, `email`, `password`, `image`, `createdAt`, `updatedAt`, `rememberToken`) VALUES
-(1, 'user', 'Gabriel', 'Martinez', 'gabmart1995', 'gabmart1995@gmail.com', '$2a$04$L2o/Wk7aTgXJL7xAfmnp0OytRrKpgr3BNYsC179NnDJ2yIgDehxxW', 'http://[::1]:3000/uploads/users/image_path-1650571036820-338207819.png', '2022-04-21 15:14:00', '2022-04-28 10:02:00', ''),
-(2, 'user', 'Alfonso', 'Martinez', 'martalf1987', 'martalf1987@gmail.com', '$2a$04$Gi/SO5TFsZE6fBXgsbV0x.A0XsPygPfvWYDT1XkOlhxf0TO5cmjve', 'http://[::1]:3000/uploads/users/image_path-1650577163177-454061274.jpeg', '2022-04-21 15:14:00', '2022-04-21 17:19:00', ''),
-(3, 'user', 'Pedro', 'Sanchez', 'p_sanchez17', 'pedro_sanchez17@gmail.com', '$2a$04$4y0MsX/sHU3naP8LeZ/hmu4f69ALsohdHRMpnF1nwC1zh7yqpiCrC', '', '2022-04-28 11:01:24', '2022-04-28 11:01:24', '');*/
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `images`
---
-ALTER TABLE `images`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `likes`
---
-ALTER TABLE `likes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_cfd8e81fac09d7339a32e57d904` (`userId`),
-  ADD KEY `FK_d2361de1b2474386782a4031731` (`imageId`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nick-unique` (`nick`),
-  ADD UNIQUE KEY `email-unique` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `comments`
---
-/*ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `images`
---
-ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT for table `likes`
---
-ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-*/
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `likes`
---
-ALTER TABLE `likes`
-  ADD CONSTRAINT `FK_cfd8e81fac09d7339a32e57d904` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `FK_d2361de1b2474386782a4031731` FOREIGN KEY (`imageId`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-31  0:52:35
